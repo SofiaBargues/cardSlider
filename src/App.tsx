470464f62c990c371d9ddc7e5d4b8eefcfeb289a
Sofia Bargues <barguessofia@gmail.com>
2024-01-30 10:44:41 +0000

    Slider

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import HomeSlider from "./componets/HomeSlider";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is home page</h1>
+      <HomeSlider />
+    </div>
+  );
+}
+
+export default App;
